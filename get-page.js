18c7ad350bc3d0862a1b453ccaba111ff2d46d48
Nick <ntomsho4545@gmail.com>
2019-06-07 10:05:07 -0400

    Add title scraping logic

--- get-page.js
+++ get-page.js
@@ -7,10 +7,16 @@ request(url, (error, response, body) => {
     if (!error) {
         const $ = cheerio.load(body);
 
+        // const shows = [];
+        // $('.pb-pl-tile-wrapper').each(function(i, show) {
+        //     shows.push($(this));
+        // })
+        // console.log($(shows[0]).children('.pb-pl-tile-title').text());
+
         const shows = [];
-        $('.pb-pl-tile-wrapper').each(function(i, show) {
-            shows.push($(this));
+        $('.pb-pl-tile-title').each(function(i, elem) {
+            shows.push({'title': $(this).text().slice(37, $(this).text().length - 33)});
         })
-        console.log($(shows[0]).children('.pb-pl-tile-title').text());
+        console.log(shows);
     }
 });
