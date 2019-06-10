02b2060b372f23960e8dc4c99d3ad626c6de09fc
Nick <ntomsho4545@gmail.com>
2019-06-10 10:27:47 -0400

    Finish first version of scraping logic

--- show-parse.js
+++ show-parse.js
@@ -4,16 +4,25 @@ const $ = require('cheerio');
 const showParse = function(url) {
     return request(url)
     .then(function(html) {
-        return {
+        let show = {
             'title': $('.bsp-bio-title', html).first().text(),
-            'type': $('.bsp-bio-sub-text', html).first().text(),
-            'genre': $('.bsp-bio-sub-text', html).first().next().text(),
+            // 'type': $('.bsp-bio-sub-text', html).first().text(),
+            // 'genre': $('.bsp-bio-sub-text', html).first().next().text(),
             'venue': $('.bsp-bio-links-top', html).first().children().first().text(),
             'venueLocation': $('.bsp-bio-links-top', html).first().children().last().text(),
             'synopsis': $('.bsp-bio-text', html).first().text(),
-
+            
             'cover': $('.bsp-bio-image', html).first().children().first().children().first().attr('src')
         };
+        show['genres'] = new Set;
+        $('.bsp-bio-sub-text', html).each(function(i, elem) {
+            show['genres'].add($(this).text());
+        })
+        $('.bsp-bio-primary-list', html).children().each(function(i, elem) {
+            const listItems = $(this).text().split(': ');
+            show[listItems[0].toLowerCase] = listItems[1];
+        })
+        return show;
     })
     .catch(function(err) {
         console.log(err);
