416a6c422ec83e56b67adef6f9851f8614c43857
Nick <ntomsho4545@gmail.com>
2019-06-11 13:29:25 -0400

    Add jsonwebtoken functionality to login function

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
 router.post('/register', (req, res) => {
     User.findOne({ email: req.body.email })
         .then(user => {
@@ -26,6 +29,38 @@ router.post('/register', (req, res) => {
             })
         }
     })
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email})
+        .then(user => {
+            if (!user) {
+                return res.status(404).jason({email: "No user found with that email"});
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        const payload = {id: user.id, name: user.name};
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            {expiresIn: 3600},
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            }
+                        )
+                    } else {
+                        return res.status(400).json({password: "Incorrect password"});
+                    }
+                })
+        })
 })
 
 module.exports = router;
